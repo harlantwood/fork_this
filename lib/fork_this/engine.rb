976edef2383f85644ac9bcd388584b362061569c
Harlan T Wood <harlan@thegoldensun.com>
2013-01-30 14:49:07 -0800

    Require polystore; minor cleanup

--- lib/fork_this/engine.rb
+++ lib/fork_this/engine.rb
@@ -1,5 +1,6 @@
 require 'haml'
 require 'formtastic'
+require 'polystore'
 
 module ForkThis
   class Engine < ::Rails::Engine
@@ -7,8 +8,8 @@ module ForkThis
     isolate_namespace ForkThis
 
     initializer "polystore" do |app|
-      stores = ENV['POLYSTORES'].to_s.split(',').compact.map(&:strip)
-      raise "Please specify stores in POLY_STORES environment variable" if stores.size < 1
+      stores = ENV['POLYSTORES'].to_s.split(',').map(&:strip)
+      raise "Please specify stores in POLYSTORES environment variable" if stores.size < 1
       config.storage = PolyStore[*stores]
     end
 
