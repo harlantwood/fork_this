7d6d490e10af17b7961fa9c642cfa2ab3c403797
Harlan T Wood <harlan@thegoldensun.com>
2013-03-02 04:37:43 -0800

    Let polystore parse the stores from the env var

--- lib/fork_this/engine.rb
+++ lib/fork_this/engine.rb
@@ -15,9 +15,7 @@ module ForkThis
     end
 
     initializer "polystore" do |app|
-      stores = ENV['POLYSTORES'].to_s.split(',').map(&:strip)
-      raise "Please specify stores in POLYSTORES environment variable" if stores.size < 1
-      config.storage = PolyStore[*stores]
+      config.storage = PolyStore.create(ENV['POLYSTORES'] || 'file')
     end
 
     initializer "domains" do |app|
