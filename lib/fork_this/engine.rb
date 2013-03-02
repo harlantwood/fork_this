5e06a7d6b923acaccf723e2fd7fe4f83d71aee29
Harlan T Wood <harlan@thegoldensun.com>
2013-03-02 04:34:22 -0800

    Redirect domain.com to www.domain.com

--- lib/fork_this/engine.rb
+++ lib/fork_this/engine.rb
@@ -1,11 +1,18 @@
 require 'haml'
 require 'formtastic'
 require 'polystore'
+require 'rack-www'
 
 module ForkThis
   class Engine < ::Rails::Engine
 
     isolate_namespace ForkThis
+
+    initializer "force_www" do |app|
+      app.middleware.use Rack::WWW, :predicate => lambda { |env|
+        env['SERVER_NAME'] == ENV['BASE_DOMAIN']
+      }
+    end
 
     initializer "polystore" do |app|
       stores = ENV['POLYSTORES'].to_s.split(',').map(&:strip)
